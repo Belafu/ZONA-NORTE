6bf58699a4a4d88cd7d2d9523d88835f23bc014d
LizaTheAnnihilator <gyupanquisalvatierra@gmail.com>
2019-04-16 23:21:22 -0300

    Cambio archivos de html a php. Subo footer lindo,su style esta en style registro

---
+++ partes/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <ul>
+    <li>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span class="fab fa-facebook-f"></span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span class="fab fa-google-plus-g"></span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span class="fab fa-linkedin-in"></span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span class="fab fa-instagram"></span>
+      </a>
+    </li>
+  </ul>
+
+
+</footer>
