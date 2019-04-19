623ef1e39cccab632e20909919940a49ef317b46
LizaTheAnnihilator <gyupanquisalvatierra@gmail.com>
2019-04-19 15:53:44 -0300

    Arreglo el footer quedo ordenado falta rellenar texto en las cajas y puse el footer en el home con include y su style del footer en la carpeta css

--- partes/footer.php
+++ partes/footer.php
@@ -1,43 +1,44 @@
-<footer>
-  <ul>
-    <li>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="fab fa-facebook-f"></span>
-      </a>
-    </li>
+  <footer>
+    <div class="caja1-footer">
+      <p class="texto-footer">Lorem ipsum dolor sit amet, consectetur adipisffff ffffffff ffffffffs ffffffff ffffffff ffffffffffffff.</p>
+    </div>
+    <div class="caja2-footer">
+      <p class="texto-footer">caja2 ipisffffff ffffffff ffffffsfffffff ffffffff ffffffffff</p>
+    </div>
+    <div class="social">
+      <div class="text-social">
+          <p>Siguenos en</p>
+      </div>
+      <div class="iconosSociales">
+        <ul>
+          <li>
+            <a href="#">
+              <span class="fab fa-facebook-f"></span>
+            </a>
+          </li>
 
-    <li>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="fab fa-google-plus-g"></span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="fab fa-linkedin-in"></span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="fab fa-instagram"></span>
-      </a>
-    </li>
-  </ul>
+          <li>
+            <a href="#">
+              <span class="fab fa-twitter"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="fab fa-google-plus-g"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="fab fa-linkedin-in"></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="fab fa-instagram"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-
-</footer>
+  </footer>
