27fcef769ab38bb7ddb5ae32c77f526aca100f59
Francodiner <49286093+Francodiner@users.noreply.github.com>
2019-04-24 12:05:57 -0300

    Home modificado
    modifico el home

---
+++ LOGIN/home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/styleHome.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <?php include("partes/header.php"); ?>
+
+    <div class="container">
+        <div id="carouselExampleFade" class="carro carousel slide carousel-fade" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://fakeimg.pl/350x200/ff0000,128/000,255" class="d-block w-100" alt="uno">
+            </div>
+            <div class="carousel-item">
+            <img src="https://fakeimg.pl/350x200/ff0000,128/000,255" class="d-block w-100" alt="dos">
+            </div>
+            <div class="carousel-item">
+            <img src="https://fakeimg.pl/350x200/ff0000,128/000,255" class="d-block w-100" alt="tres">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+    </div>
+
+<div class="container">
+  <article class="product">
+    <img src="https://fakeimg.pl/350x300/">
+    <img src="https://fakeimg.pl/350x300/">
+    <img src="https://fakeimg.pl/350x300/">
+  </article>
+</div>
+
+<div class="container">
+  <section class="info">
+    <div class="card">
+    <img src="https://fakeimg.pl/350x200/">
+    <div class="card-body">
+      <h5 class="card-title">Mouse Gamer</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      <a href="#" class="btn btn-primary">Ver mas</a>
+      </div>
+      </div>
+      <div class="card">
+      <img src="https://fakeimg.pl/350x200/">
+      <div class="card-body">
+        <h5 class="card-title">Mouse Gamer</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="#" class="btn btn-primary">Ver mas</a>
+        </div>
+        </div>
+        <div class="card">
+        <img src="https://fakeimg.pl/350x200/">
+        <div class="card-body">
+          <h5 class="card-title">Mouse Gamer</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <a href="#" class="btn btn-primary">Ver mas</a>
+          </div>
+          </div>
+  </section>
+
+</div>
+<marquee class="marque"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"></marquee>
+
+  <?php include("partes/footer.php"); ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
