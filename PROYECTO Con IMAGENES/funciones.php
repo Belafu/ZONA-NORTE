9bebc7fafd4c551503b95084be0dab822504cc8e
Your Name <you@example.com>
2019-05-17 19:51:46 -0300

    Subo Home y Productos terminado con imagenes y todo responsive

---
+++ PROYECTO Con IMAGENES/funciones.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+function validarRegistro($datos){
+    $errores=[];
+    $datosFinales = [];
+    foreach ($datos as $posicion => $valores) {//todo el foreach es para trimiar los datos (osea que no queden espacios a los costados)
+    if(!is_array($datos[$posicion])){
+        $datosFinales[$posicion] = trim($valores);
+    }
+      // if($posicion !== "hobbies"){
+      //   $datosFinales[$posicion] = trim($valores);
+      // }
+    } // ver en que caso rompe.... Rta: cuando el dato es un array.
+    //var_dump($errores, $datosFinales);
+    if(strlen($datosFinales["nombre"]) == 0){
+      $errores["nombre"] = "Por favor complete el campo nombre.";
+    } else if(ctype_alpha($datosFinales["nombre"]) == false){
+      $errores["nombre"] = "El nombre debe contener solo letras";
+    }
+    if(strlen($datosFinales["email"]) == 0){
+      $errores["email"] = "Por favor complete el campo email.";
+    } elseif (!filter_var($datosFinales["email"], FILTER_VALIDATE_EMAIL)) {
+      $errores["email"] = "Por favor ingrese un email con formato válido.";
+    }
+    /*PENDIENTEEEEEEEEEEEEEE
+    if ( validar si no hay otro usuario con el mismo name ) {
+      $errores["username"]="Este nombre de Usuario ya esta siendo usado "
+    }*/
+    if(strlen($datosFinales["password"]) == 0){
+      $errores["password"] = "El campo contraseña no puede estar vacío.";
+    }
+    /*EN CASO QUIERAS QUE REPITA SU CONTRASEÑA 2 VECES (SERA PARA MAYOR SEGURIDAD)
+    if(strlen($datosFinales["pass2"]) == 0){
+      $errores["pass"] = "Por favor repita su contraseña.";
+    } elseif($datosFinales["pass"] !== $datosFinales["pass2"]){
+      $errores["pass"] = "Las contraseñas no coinciden.";
+    }*/
+//VALIDACION DE LA IMAGEN
+    //NO QUIERO QUE SEA UN NECESARIO CARGAR LA IMAGEN
+    /*
+    if ($_FILES["foto"]["error"]!= 0) {
+      $errores["foto"] = "Hubo un error al acargar la imagen <br>";
+    }  else {
+        $ext = pathinfo($_FILES["foto"]["name"],PATHINFO_EXTENSION);
+        if ($ext!= "jpg" && $ext!= "jpeg" && $ext!= "png") {
+            $errores["foto"] = "el cv debe ser jpg ,jpeg o png <br>";
+        }
+    }
+    */
+    if (buscarClientePorUsuario($_POST["username"]) != NULL) {
+      $errores["username"] = "Nombre de Usuario usado";
+    }
+    return $errores;
+}
+function nextId(){
+  // TODO: que pasa si no hay usuario anterior.
+  if (!file_exists("usuarios.json")) {
+    $json = '';
+  }else {
+    $json = file_get_contents("usuarios.json");
+  }
+  if ($json == '') {
+    return 1;
+  }
+  $array = json_decode($json, true);
+  $ultimoUsuario = array_pop($array["usuarios"]);
+  $lastId = $ultimoUsuario["id"];
+  return $lastId + 1;
+}
+function armarUsuario(){//Se pasa por $_POST
+  return  [
+    "id" => nextId(),
+    "nombre" => trim($_POST["nombre"]),
+    "email" =>  trim($_POST["email"]),
+    "username" => trim($_POST["username"]),
+    "password" => password_hash($_POST["password"],PASSWORD_DEFAULT),
+    "gender" => trim($_POST["gender"]),
+    "foto" => "img/".$_POST["username"]
+  ];
+}
+function guardarUsuario($usuario){
+  // TODO: que pasa si no hay archivo.
+  if (!file_exists("usuarios.json")) {
+    $json = '';
+  }else {
+    $json = file_get_contents("usuarios.json");
+  }
+//  $json = file_get_contents("usuarios.json");
+  $array = json_decode($json, true);
+  $array["usuarios"][] = $usuario;
+  $array = json_encode($array, JSON_PRETTY_PRINT);
+  file_put_contents("usuarios.json", $array);
+}
+//Falta validar que no haya usuarios repetidos ese es nuestro criterio
+function buscarClientePorUsuario($username){
+  $json = file_get_contents("usuarios.json");
+  $array = json_decode($json, true);
+  foreach ($array["usuarios"] as $value) {
+    if ($value["username"]==$username) {
+      return $value;
+    }
+  }
+  return null;
+}
+function existeUsuario($username){
+  return buscarClientePorUsuario($username) !== null;
+}
+function validarLogin($datos){
+  $errores = [];
+  if(strlen($datos["username"]) == 0){
+    $errores["username"] = "Por favor complete el campo email.";
+  } elseif(!existeUsuario($datos["username"])){
+    $errores["username"] = "Este Username no se encuentra registrado.";
+  }
+  if(strlen($datos["password"]) == 0){
+    $errores["password"] = "El campo contraseña no puede estar vacío.";
+  } else {
+    $usuario = buscarClientePorUsuario($datos["username"]);
+    if(!password_verify($datos["password"], $usuario["password"])){
+      $errores["password"] = "La contraseña es incorrecta.";
+    }
+  }
+  return $errores;
+}
+function loguearUsuario($username){
+  $_SESSION["username"] = $username;
+}
+function usuarioLogueado(){
+  return isset($_SESSION["username"]);
+}
+function traerUsuarioLogueado(){
+  // Si está logueado trae los datos del usuario
+  if(isset($_SESSION["username"])) {
+    $usuario = buscarClientePorUsuario($_SESSION["username"]);
+    return $usuario;
+  } else {
+    // Sino: FALSE
+    return false;
+  }
+}
+function listaUsuarios(){
+  $json = file_get_contents("usuarios.json");
+  $array = json_decode($json, true);
+  return $array["usuarios"];
+}
+ ?>
---
+++ PROYECTO Con IMAGENES/home.php
@@ -0,0 +1,145 @@
+<?php
+require 'funciones.php';
+$usuario = traerUsuarioLogueado();//SIERVE PARA TRAER SU IMAGEN Y DEMAS DATOS
+$usuarioLogueado = usuarioLogueado();
+var_dump($_SESSION);
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/estiloHome.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <?php include("partes/header.php"); ?>
+
+
+    <div class="container">
+      <section "carro">
+        <h1>NOVEDADES //</h1>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="productos.php">
+        <img class="imagen-carro w-50" src="img/mouse2.png" alt="First slide">
+      </a>
+    </div>
+    <div class="carousel-item">
+      <a href="productos.php">
+      <img class="imagen-carro w-50" src="img/teclado3.png" alt="Second slide">
+      </a>
+    </div>
+    <div class="carousel-item">
+      <img class="imagen-carro w-50" src="img/auricular.png" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+      </section>
+
+      </div>
+
+      <div class="container">
+        <article class="informacion">
+          <div class="info-seguridad">
+            <img class="seguridad" src="img/seguridad.png" alt="" width="130px" height="130px">
+            <div class="data-seguridad">
+            </div>
+          </div>
+          <div class="info-seguridad">
+            <img class="seguridad" src="img/tarjeta.png" alt="" width="130px" height="130px">
+            <div class="data-seguridad">
+            </div>
+          </div>
+          <div class="info-seguridad">
+            <img class="seguridad" src="img/camion.png" alt="" width="130px" height="130px">
+            <div class="data-seguridad">
+            </div>
+          </div>
+
+          </div>
+
+        </article>
+      </div>
+
+<div class="container">
+  <h1 class="ofertasdelmes">OFERTAS //</h1>
+  <section class="product-container">
+    <article class="product-logo">
+      <a href="productos.php">
+        <img class="product-img-ofertas img-fluid" src="img/mouse3.png" alt="" width="325px" height="300px">
+      </a>
+      <img class="promo img-fluid"src="img/descuento.png" alt="" width="100px" height="50px">
+    </article>
+    <article class="product-logo">
+      <a href="productos.php">
+        <img class="product-img-ofertas img-fluid" src="img/teclado.png" alt="" width="325px" height="300px">
+      </a>
+      <img class="oferta img-fluid"src="img/oferta.png" alt="" width="140px" height="110px">
+    </article>
+    <article class="product-logo">
+      <a href="productos.php">
+        <img class="product-img-ofertas img-fluid" src="img/auricular2.png" alt="" width="325px" height="300px">
+      </a>
+      <img class="ofertita img-fluid"src="img/ofertita.png" alt="" width="80px" height="40px">
+    </article>
+</div>
+
+<div class="container">
+  <h1 class="ofertasdelmes">INGRESOS //</h1>
+  <section class="info">
+    <div class="card">
+    <img class="product-img img-fluid" src="img/mouse.png" alt="" width="350px" height="200px">
+    <div class="card-body">
+      <h5 class="card-title">Mouse Gamer</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      <a href="productos.php" class="btn btn-primary">Ver mas</a>
+      </div>
+      </div>
+      <div class="card">
+      <img class="product-img img-fluid" src="img/auricular3.png" alt="" width="350px" height="200px">
+      <div class="card-body">
+        <h5 class="card-title">Auricular Gamer</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="productos.php" class="btn btn-primary">Ver mas</a>
+        </div>
+        </div>
+        <div class="card">
+        <img class="product-img img-fluid" src="img/teclado2.png" alt="" width="350px" height="200px">
+        <div class="card-body">
+          <h5 class="card-title">Teclado Gamer</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <a href="productos.php" class="btn btn-primary">Ver mas</a>
+          </div>
+          </div>
+  </section>
+
+</div>
+<marquee class="marque"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"><img class="fotos-marque" src="https://fakeimg.pl/300x100/"></marquee>
+
+  <?php include("partes/footer.php"); ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
