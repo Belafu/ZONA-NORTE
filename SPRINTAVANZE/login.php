878ff1db52f785d67a2757aa9f32822e9bc916b0
DigitalHouse <francodiner1232011@gmail.com>
2019-05-29 12:22:33 -0300

    avanze del tercer sprint con base de datos creada por mi y foot y header corregidos con colores iconos, etc

---
+++ SPRINTAVANZE/login.php
@@ -0,0 +1,77 @@
+<?php
+require_once "funciones.php";
+
+if(usuarioLogueado()){
+  header("Location:home.php");
+  exit;
+}
+$userOK='';
+$errores = [];
+  //Si viene por POST
+if($_POST){
+
+    $userOK = $_POST["username"];
+    $errores = validarLogin($_POST);
+
+    if(empty($errores)){
+        if (isset($_POST["recordar"])) {
+            setcookie("username",$userOK,time()+15);//15 segundos
+        }
+      loguearUsuario($_POST["username"]);
+
+      //var_dump($_SESSION);NO SE PORQUE SI DEJO ESTO SALE UN WARNIGN??
+      header("Location:home.php");//redirigimos a home
+      exit;
+    }/*Si quieres ver los erroes arriba
+    else {
+      var_dump($_POST);
+    }*/
+}
+
+?>
+
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/styleLogin.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include("partes/header.php"); ?>
+    <div class="login-page">
+      <div class="form">
+        <form class="login-form" action="login.php" method="post" enctype="multipart/form-data">
+             <label for="Usuario"></label>
+             <input class="campo-login" id="usuario" name="username" type="text" value='<?= $userOK ?>' placeholder="Usuario"/>
+             <?php if(isset($errores["username"])): ?>
+               <span class="small text-danger"><?= $errores["username"] ?></span>
+             <?php endif ?>
+
+             <label for="pass"></label>
+             <input class="campo-login" id="pass" type="password" name="password" placeholder="Contraseña"/>
+             <?php if(isset($errores["password"])): ?>
+               <span class="small text-danger"><?= $errores["password"] ?></span>
+             <?php endif ?>
+
+             <div class="recordar">
+                <input class="checkbox-login" type="checkbox" name="recordar" value="">
+                <label class="texto-recordar" for="recordar">Recordarme</label>
+              </div>
+             <button>ingresar</button>
+          <p class="message">Aun no estas registrado?<a class="reg-button" href="registro.php"> Registrarse</a></p>
+        </form>
+      </div>
+    </div>
+    <?php include("partes/footer.php"); ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
